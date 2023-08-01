bc0a20634d71b22e74d15d8fc0e2143f027c6287
m-navaneeth <141069800+m-navaneeth@users.noreply.github.com>
2023-08-01 02:31:27 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDE94wzJIFHChziKPQepyxr7l-MAKJj988",
-  authDomain: "blok-dev.firebaseapp.com",
-  projectId: "blok-dev",
-  storageBucket: "blok-dev.appspot.com",
-  messagingSenderId: "393520501802",
-  appId: "1:393520501802:web:3c8f01e2151be2d3ad9f53",
-  measurementId: "G-5L6FEPZDGF"
+
 };
 
 // Initialize Firebase
